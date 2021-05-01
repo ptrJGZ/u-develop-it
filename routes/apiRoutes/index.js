1844eba8bd87e3b1b159969a9c48ec60bacec19d
Peter J. Gonzalez <ptr.jgz@gmail.com>
2021-05-01 04:59:24 -0400

    Create route to get vote totals

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { route } = require('./candidateRoutes');
 const router = express.Router();
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
